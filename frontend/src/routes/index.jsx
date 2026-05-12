3a6f7b9cfd0bce00cf6147b3295a528e39939447
Phuc <bkphuc2003@bkphuc2003gmail.com>
2026-05-12 23:49:13 +0700

    add student and lecture list page, add context for authorization

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -1,30 +1,36 @@
 import { Suspense, lazy } from "react";
-import { Navigate, useRoutes, useLocation } from "react-router-dom";
+import { Navigate, useRoutes } from "react-router-dom";
 // components
 import LoadingScreen from "../components/LoadingScreen";
 
 // ----------------------------------------------------------------------
 
-const Loadable = (Component) => (props) => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const { pathname } = useLocation();
+const Loadable = (Component) => {
+  const WrappedComponent = (props) => {
+    //const { pathname } = useLocation();
+    return (
+      <Suspense fallback={<LoadingScreen />}>
+        <Component {...props} />
+      </Suspense>
+    );
+  };
+  
+  WrappedComponent.displayName = `Loadable(${Component.name || "Component"})`;
 
-  return (
-    <Suspense fallback={<LoadingScreen />}>
-      <Component {...props} />
-    </Suspense>
-  );
+
+  return WrappedComponent;
 };
+
 
 export default function Router() {
   return useRoutes([
     {
-      path: "authentication",
+      path: "auth",
       children: [
         { path: "login", element: <Login /> },
-        { path: "register", element: <Register /> },
+        // { path: "register", element: <Register /> },
         { path: "forgot-password", element: <ForgotPassword /> },
-        { path: "verify", element: <VerifyCode /> },
+        // { path: "verify", element: <VerifyCode /> },
       ],
     },
     // Main Routes
@@ -38,6 +44,22 @@ export default function Router() {
         { path: "500", element: <Page500 /> },
         { path: "404", element: <Page404 /> },
         { path: "*", element: <Navigate to="/404" replace /> },
+        { path: "faqs", element: <FAQs/>},
+        { path: "test", element: <TestPage/>},
+      ],
+    },
+    {
+      path: "student",
+      children: [
+        { path: "studentClassRegistration", element: <StudentClassRegistrationPage/>},
+        { path: "studentClassAndAssignment", element: <StudentClassAndAssignmentPage/>},
+      ],
+    },
+    {
+      path: "ministry",
+      children: [
+        { path: "studentList", element: <StudentListPage /> },
+        { path: "lecturerList", element: <LecturerListPage /> },
       ],
     },
   ]);
@@ -45,15 +67,15 @@ export default function Router() {
 
 // AUTHENTICATION
 const Login = Loadable(lazy(() => import("../pages/authentication/Login")));
-const Register = Loadable(
-  lazy(() => import("../pages/authentication/Register"))
-);
+// const Register = Loadable(
+//   lazy(() => import("../pages/authentication/Register"))
+// );
 const ForgotPassword = Loadable(
   lazy(() => import("../pages/authentication/ForgotPassword"))
 );
-const VerifyCode = Loadable(
-  lazy(() => import("../pages/authentication/VerifyCode"))
-);
+// const VerifyCode = Loadable(
+//   lazy(() => import("../pages/authentication/VerifyCode"))
+// );
 
 // MAIN
 const Page500 = Loadable(lazy(() => import("../pages/Page500")));
@@ -61,3 +83,14 @@ const Page404 = Loadable(lazy(() => import("../pages/Page404")));
 const AboutUs = Loadable(lazy(() => import("../pages/AboutUs")));
 const ComingSoon = Loadable(lazy(() => import("../pages/ComingSoon")));
 const Maintenance = Loadable(lazy(() => import("../pages/Maintenance")));
+const FAQs = Loadable(lazy(() => import("../pages/Faqs")));
+
+const TestPage = Loadable(lazy(() => import("../pages/TestPage")));
+
+// STUDENT
+const StudentClassRegistrationPage = Loadable(lazy(() => import("../pages/Student/StudentClassRegistration")));
+const StudentClassAndAssignmentPage =  Loadable(lazy(() => import("../pages/Student/StudentClassesAndAssignmentsPage")));
+
+// MINISTRY
+const StudentListPage = Loadable(lazy(() => import("../pages/ministry/StudentListPage")));
+const LecturerListPage = Loadable(lazy(() => import("../pages/ministry/LecturerListPage")));
